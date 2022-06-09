bd146e1a564435fcaf05f375e62c8a091cd5017f
lauratro <lauratronchin@hotmail.it>
2022-06-09 20:16:04 +0200

    Add Context in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import Login from "./components/Login/Login";
 import Header from "./components/Header/Header";
 import CoursesList from "./components/CoursesList/CoursesList";
+import VariableProvider from './context/VariableContext'
 
 import "./App.css";
 
 function App() {
   return (
+    <VariableProvider>
     <div className="App">
       <Header />
       <Login />
       <CoursesList />
     </div>
+    </VariableProvider>
   );
 }
 
