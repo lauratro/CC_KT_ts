c9525d0e21af12cdaca4fa9037c1bd2a42c7a762
lauratro <lauratronchin@hotmail.it>
2022-06-09 14:57:03 +0200

    Create Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header:React.FC=()=> {
+  return (
+     <div className="headerCont">
+    <div className='companyName'>INFECTOPHARM</div>
+</div>
+  )
+}
+export default Header;
