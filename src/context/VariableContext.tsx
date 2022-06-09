5aea916fc97ba1b1eccefc4f66a8f994ad0c6148
lauratro <lauratronchin@hotmail.it>
2022-06-09 20:19:53 +0200

    Remove createContext

--- src/context/VariableContext.tsx
+++ src/context/VariableContext.tsx
@@ -1,5 +1,5 @@
-//import * as React from 'react';
-import React, {useState, useContext, createContext} from "react"
+
+import React, {useState, useContext} from "react"
 
 export interface Variables {
    isLoggedIn:boolean,
