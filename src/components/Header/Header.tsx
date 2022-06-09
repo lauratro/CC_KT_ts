7d24497c8e30c781e45edf2280a3d2a96bf683a2
lauratro <lauratronchin@hotmail.it>
2022-06-09 20:15:47 +0200

    Import logout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./Header.css"
+import Logout from "./../Logout/Logout"
 
 const Header:React.FC=()=> {
   return (
      <div className="headerCont">
     <div className='companyName'>INFECTOPHARM</div>
+    <Logout />
 </div>
   )
 }
