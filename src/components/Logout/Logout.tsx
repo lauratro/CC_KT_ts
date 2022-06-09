0f99c4f0082546ee02966279418eb26d5e5ab87f
lauratro <lauratronchin@hotmail.it>
2022-06-09 20:18:25 +0200

    Remove unused import and variables

--- src/components/Logout/Logout.tsx
+++ src/components/Logout/Logout.tsx
@@ -1,21 +1,14 @@
-import React,{useContext,useEffect} from 'react'
-import VariableContext from '../../context/VariableContext'
+import React from 'react'
+
 import {useGlobalContext} from "../../context/VariableContext"
 
-export interface Variables {
-    isLoggedIn:boolean,
-    logout:()=>void,
-    login:()=>void,
-    setIsLoggedIn:React.Dispatch<React.SetStateAction<boolean>>
-    children?: React.ReactNode
-   }
+
 const Logout:React.FC=()=> {
-    const userLogged = localStorage.getItem("isLogged")
+   
     const {isLoggedIn, logout } = useGlobalContext()
-   // const appContext = () => useContext(VariableContext)
-    //const { isLoggedIn,setIsLoggedIn } = appContext as Variables
+
     
-    console.log(isLoggedIn)
+
 
     const logOut =()=>{
        logout()
