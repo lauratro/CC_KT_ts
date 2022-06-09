15ad4e2f9e3442d60d19bceac0e827bbb39c113c
lauratro <lauratronchin@hotmail.it>
2022-06-09 12:02:25 +0200

    Write initial structure of the mutation without ts

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -3,13 +3,31 @@ import { CREATE_AUTH_MUTATION } from '../../Graphql/Mutation';
 import { useMutation } from '@apollo/client';
 
 const Login:React.FC =()=> {
- 
+    const getData=(data)=>{
+        localStorage.setItem('token', data.Auth.login.token );
+        localStorage.setItem('userId',data.Auth.login.userId);
+      
+      }
+      const [createAuth, { data, loading, error }] = useMutation(CREATE_AUTH_MUTATION,{variables:{
+    email:"karl.kroeber@thekey.technology",password:"testtest"
+      }})
+    
+      
+    
+      if(data)getData(data)
     
       return (
         <div>
-      
+          <div style={{textAlign:"center"}}>
+          {loading && <p>Submitting....</p>}
+          {error && <p>Submission error! ${error.message}</p> }
+          </div>
         <div>
-            <form  className='formContainer' >
+            <form  className='formContainer'  onSubmit={(e) => {
+            e.preventDefault();
+            createAuth();
+         
+          }} >
     <div className='inputField'>
       <label className='labelStyle' htmlFor='username'>Username</label>
       <input   type="text" defaultValue={"xxxxxxxx"}  />
