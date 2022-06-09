39637eae9a797ad81edd70bd23be51b3f7f79f4a
lauratro <lauratronchin@hotmail.it>
2022-06-09 14:53:23 +0200

    Set function with typescript

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,81 +1,98 @@
-import React, {useState} from 'react'
-import { CREATE_AUTH_MUTATION } from '../../Graphql/Mutation';
-import { useMutation } from '@apollo/client';
+import React, { useState } from "react";
+import { CREATE_AUTH_MUTATION } from "../../Graphql/Mutation";
+import { useMutation } from "@apollo/client";
 
-import "./Login.css"
+import "./Login.css";
 
+interface AuthResult {
+    
+        
+          login: {
+            token: string,
+            userId: string
+          }
+        
+      
+  }
 
 interface FormData {
-    email: string;
-    password: string;
-    
-  }
-const Login:React.FC =()=> {
-    const[input,setInput]=useState<FormData>({
-        email:"karl.kroeber@thekey.technology",
-        password:"testtest",
-    })
-/*     const getData=(data:any)=>{
-        localStorage.setItem('token', data.Auth.login.token );
-        localStorage.setItem('userId',data.Auth.login.userId);
-      
-      } */
-     
+  email: string;
+  password: string;
+ Auth?:AuthResult
 
-      const [createAuth, { data, loading, error }] = useMutation<FormData>(CREATE_AUTH_MUTATION)
-    
-      const handleCLick = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (!input.email || !input.password) {
-          alert('Enter email and password!');
-        } else {
-          console.log(input.email);
-          createAuth({
-            variables: {
-              email: input.email,
-              password: input.password,
-            },
-          })
-            .then(({ data }) => {
-             
-              console.log(data);
+}
+
+
+const Login: React.FC = () => {
+  const [input, setInput] = useState<FormData>({
+    email: "karl.kroeber@thekey.technology",
+    password: "testtest",
+  });
+
+
+  const [createAuth, { error }] = useMutation<FormData>(CREATE_AUTH_MUTATION);
+
+  const handleCLick = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!input.email || !input.password) {
+      alert("Enter email and password!");
+    } else {
+      
+      createAuth({
+        variables:{ 
+          email: input.email,
+          password: input.password,
+        },
+      })
+        .then(({ data}) => {
+          if (data?.Auth?.login !== undefined && data.Auth.login !== null) {
+            localStorage.setItem('token', data?.Auth?.login.token  );
+            localStorage.setItem('userId', data?.Auth?.login.userId );
+          } 
+     
         
-            })
-            .catch((error) => {
-              console.log(error);
-            });
-          if (error) {
-            console.log(error);
-          } else {
-            console.log('user logged in');
-          }
-        }
-      };    
-    
-/*       if(data)getData(data)
-    console.log(typeof data) */
-      return (
-        <div>
-          <div style={{textAlign:"center"}}>
-          {loading && <p>Submitting....</p>}
-          {error && <p>Submission error! ${error.message}</p> }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("user logged in");
+      }
+    }
+  };
+
+  return (
+    <div>
+      <div style={{ textAlign: "center" }}>
+        {error && <p>Submission error! ${error.message}</p>}
+      </div>
+      <div>
+        <form className="formContainer" onSubmit={handleCLick}>
+          <div className="inputField">
+            <label className="labelStyle" htmlFor="username">
+              Username
+            </label>
+            <input
+              type="text"
+              defaultValue={"karl.kroeber@thekey.technology"}
+            />
           </div>
-        <div>
-            <form  className='formContainer'  >
-    <div className='inputField'>
-      <label className='labelStyle' htmlFor='username'>Username</label>
-      <input   type="text" defaultValue={"karl.kroeber@thekey.technology"}   />
+          <div className="inputField">
+            <label className="labelStyle" htmlFor="passwort">
+              Passwort
+            </label>
+            <input type="text" defaultValue={"testtest"} />
+          </div>
+          <div className="buttonCont">
+            <button className="submitButton" type="submit">
+              Log in
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-    <div className='inputField'>
-      <label className='labelStyle' htmlFor='passwort'>Passwort</label>
-      <input type="text" defaultValue={"testtest"}  />
-    </div>
-    <div className='buttonCont'>
-    <button className='submitButton' type="submit">Log in</button>
-    </div>
-            </form>
-       
-        </div>
-        </div>
-      )}
+  );
+};
 export default Login;
