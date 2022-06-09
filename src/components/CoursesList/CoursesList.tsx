2b3acb035ae182c0f23f6d9685a896116d075bed
lauratro <lauratronchin@hotmail.it>
2022-06-09 15:07:37 +0200

    Add text from other repository

--- src/components/CoursesList/CoursesList.tsx
+++ src/components/CoursesList/CoursesList.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React,{useState} from "react";
+import { useQuery } from "@apollo/client";
+import {GET_LIST} from "./../../Graphql/Query"
+
 
 const CoursesList:React.FC = ()=> {
-  return (
-    <div>CoursesList</div>
-  )
+    const[courses, setCourses]=useState([])
+    let token = localStorage.getItem("token");
+    let id = localStorage.getItem("id");
+
+    const { loading, error, data } = useQuery(GET_LIST);
+  
+   
+    if (data) console.log(data);
+    return <div> {loading && <p>loading</p>}
+    {error !== undefined && <p>{error.message}</p>}
+    {data !== undefined &&
+      courses.map(course => {
+        return (
+       <div>
+         <img src={course.url} />
+         <p>{course.title}</p>
+       </div>
+        );
+      })}</div>;
 }
 export default CoursesList
