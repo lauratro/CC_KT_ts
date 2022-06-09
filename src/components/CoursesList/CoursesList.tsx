e059e0dc5617c8ca89a38ddceae6f7434a6995bd
lauratro <lauratronchin@hotmail.it>
2022-06-09 17:31:20 +0200

    Remove variables

--- src/components/CoursesList/CoursesList.tsx
+++ src/components/CoursesList/CoursesList.tsx
@@ -9,20 +9,20 @@ interface Course{
 
 const CoursesList:React.FC = ()=> {
     const[courses, setCourses]=useState<Course[]>([])
-    let token = localStorage.getItem("token");
-    let id = localStorage.getItem("id");
-
-    const { loading, error, data } = useQuery(GET_LIST);
+  
+    const { loading, error, data } = useQuery(GET_LIST,{variables:{tags:localStorage.getItem("userId")}});
   
    
-    if (data) console.log(data);
-    return <div> {loading && <p>loading</p>}
+
+    return <div> 
+        <h2>Kursen</h2>
+        {loading && <p>loading</p>}
     {error !== undefined && <p>{error.message}</p>}
     {data !== undefined &&
       courses.map(course => {
         return (
        <div>
-         <img src={course.icon.url} />
+         <img alt="icon" src={course.icon.url} />
          <p>{course.title}</p>
        </div>
         );
