9b1c3562d06c009d3734d1caa51668ea6750f761
lauratro <lauratronchin@hotmail.it>
2022-06-09 12:28:45 +0200

    Write function to run mutation

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { CREATE_AUTH_MUTATION } from '../../Graphql/Mutation';
 import { useMutation } from '@apollo/client';
+
+import "./Login.css"
 
 
 interface FormData {
@@ -9,19 +11,49 @@ interface FormData {
     
   }
 const Login:React.FC =()=> {
-    const getData=(data)=>{
+    const[input,setInput]=useState<FormData>({
+        email:"karl.kroeber@thekey.technology",
+        password:"testtest",
+    })
+/*     const getData=(data:any)=>{
         localStorage.setItem('token', data.Auth.login.token );
         localStorage.setItem('userId',data.Auth.login.userId);
       
-      }
-      const [createAuth, { data, loading, error }] = useMutation(CREATE_AUTH_MUTATION,{variables:{
-    email:"karl.kroeber@thekey.technology",password:"testtest"
-      }})
+      } */
+     
+
+      const [createAuth, { data, loading, error }] = useMutation<FormData>(CREATE_AUTH_MUTATION)
     
-      
+      const handleCLick = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!input.email || !input.password) {
+          alert('Enter email and password!');
+        } else {
+          console.log(input.email);
+          createAuth({
+            variables: {
+              email: input.email,
+              password: input.password,
+            },
+          })
+            .then(({ data }) => {
+             
+              console.log(data);
+        
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('user logged in');
+          }
+        }
+      };    
     
-      if(data)getData(data)
-    
+/*       if(data)getData(data)
+    console.log(typeof data) */
       return (
         <div>
           <div style={{textAlign:"center"}}>
@@ -29,18 +61,14 @@ const Login:React.FC =()=> {
           {error && <p>Submission error! ${error.message}</p> }
           </div>
         <div>
-            <form  className='formContainer'  onSubmit={(e) => {
-            e.preventDefault();
-            createAuth();
-         
-          }} >
+            <form  className='formContainer'  >
     <div className='inputField'>
       <label className='labelStyle' htmlFor='username'>Username</label>
-      <input   type="text" defaultValue={"xxxxxxxx"}  />
+      <input   type="text" defaultValue={"karl.kroeber@thekey.technology"}   />
     </div>
     <div className='inputField'>
       <label className='labelStyle' htmlFor='passwort'>Passwort</label>
-      <input type="text" defaultValue={"xxxxxx"}  />
+      <input type="text" defaultValue={"testtest"}  />
     </div>
     <div className='buttonCont'>
     <button className='submitButton' type="submit">Log in</button>
