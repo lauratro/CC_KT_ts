706542ede004931be007d321758160025b5ff871
lauratro <lauratronchin@hotmail.it>
2022-06-10 18:56:14 +0200

    Change interface and array to map

--- src/components/CoursesList/CoursesList.tsx
+++ src/components/CoursesList/CoursesList.tsx
@@ -5,13 +5,16 @@ import Loading from "./../Loading/Loading";
 import {useGlobalContext} from "../../context/VariableContext"
 import "./CoursesList.css"
 interface Course {
+node:{
   icon: { url: string };
   title: "string";
+}
 }
 
 const CoursesList: React.FC = () => {
   const { loading, error, data } = useQuery(GET_LIST);
   const {isLoggedIn } = useGlobalContext()
+  if(data)console.log(data.Learn.LearnOpportunities.edges)
   return (
     <div>
       {isLoggedIn &&
@@ -19,15 +22,15 @@ const CoursesList: React.FC = () => {
       <h2 className="coursesTitle">Kurse</h2>
       {loading && <Loading />}
       {error !== undefined && <p>{error.message}</p>}
-      {data !== undefined &&
-        data.map((course: Course) => {
+     {data.Learn.LearnOpportunities !== undefined &&
+        data.Learn.LearnOpportunities.edges.map((course: Course) => {
           return (
-            <div>
-              <img alt="icon" src={course.icon.url} />
-              <p>{course.title}</p>
+            <div key={course.node.title}>
+              <img  alt="icon" src={course.node.icon.url} />
+              <p>{course.node.title}</p>
             </div>
           );
-        })}
+        })}  
     </div>
 }
     </div>
