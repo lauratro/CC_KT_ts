67338485b28d409b4c3f35908bfa1f25de5d5d2e
lauratro <lauratronchin@hotmail.it>
2022-06-11 10:50:14 +0200

    Format code

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 const client = new ApolloClient({
-  uri: 'https://staging.api.wissenwirkt.com/api/graphql',
+  uri: "https://staging.api.wissenwirkt.com/api/graphql",
   cache: new InMemoryCache(),
- headers: {
-
-    "X-Auth-Token":localStorage.getItem('token') ||"6718a44c-8813-460f-a8e1-70a0942582a6",
-   "X-Auth-Account-Id":localStorage.getItem("userId") || "VXNlcjpVc2VyOmJlZWY5NWEzLTYxNjgtNDIzNy04NDc0LTE0YWIyZjk1ZTI5Nw=="  
-} ,
-connectToDevTools: true
+  headers: {
+    "X-Auth-Token": localStorage.getItem("token") || "",
+    "X-Auth-Account-Id": localStorage.getItem("userId") || "",
+  },
+  connectToDevTools: true,
 });
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-     <ApolloProvider client={client}>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
     </ApolloProvider>
   </React.StrictMode>
 );
