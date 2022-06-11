52b1403693f07f7cbb74640137f6e407a6189836
lauratro <lauratronchin@hotmail.it>
2022-06-11 10:17:13 +0200

    Text searchbar

--- src/components/CoursesList/CoursesList.tsx
+++ src/components/CoursesList/CoursesList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { useQuery } from "@apollo/client";
 import { GET_LIST } from "./../../Graphql/Query";
 import Loading from "./../Loading/Loading";
@@ -10,29 +10,41 @@ node:{
   title: "string";
 }
 }
+interface CoursesList{
+ edges:Course[]
+}
+
 
 const CoursesList: React.FC = () => {
+
+const [searchedText,setSearchedText]= useState<string>("")
   const { loading, error, data } = useQuery(GET_LIST);
   const {isLoggedIn } = useGlobalContext()
-  if(data)console.log(data.Learn.LearnOpportunities.edges)
+
+ console.log(searchedText)
+ const filtered:Course[] = searchedText ? data.Learn.LearnOpportunities.edges.filter((course:Course)=>{
+ return course.node.title.toLowerCase().includes(searchedText.toLowerCase())
+ }):data.Learn.LearnOpportunities.edges
+
   return (
     <div>
-      {isLoggedIn &&
+     
       <div className="container">
       <h2 className="coursesTitle">Kurse</h2>
+      <input type="text" value={searchedText} onChange={(e)=>setSearchedText(e.target.value)}/>
       {loading && <Loading />}
       {error !== undefined && <p>{error.message}</p>}
-     {data.Learn.LearnOpportunities !== undefined &&
-        data.Learn.LearnOpportunities.edges.map((course: Course) => {
+            {data !== undefined &&
+        filtered.map((course: Course) => {
           return (
             <div key={course.node.title}>
               <img  alt="icon" src={course.node.icon.url} />
               <p>{course.node.title}</p>
             </div>
           );
-        })}  
+        })}   
     </div>
-}
+
     </div>
   );
 };
