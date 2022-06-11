e70657524e9ee68477f9db00d656b37b313f56f9
lauratro <lauratronchin@hotmail.it>
2022-06-11 09:57:41 +0200

    Add useEffect

--- src/components/Logout/Logout.tsx
+++ src/components/Logout/Logout.tsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import "./Logout.css"
 
 import {useGlobalContext} from "../../context/VariableContext"
-
+import {
+  useNavigate
+} from "react-router-dom";
+import Login from '../Login/Login';
 
 const Logout:React.FC=()=> {
    
-    const {isLoggedIn, logout } = useGlobalContext()
-
-    
-
+    const {isLoggedIn, logout, login } = useGlobalContext()
+const token = localStorage.getItem("token")
+  const navigate = useNavigate()  
+useEffect(()=>{
+  if(token !== ""){
+    login()
+  }
+},[token])
 
     const logOut =()=>{
        logout()
+       localStorage.clear()
+       navigate("/")
+
     }
   return (
     <div>
