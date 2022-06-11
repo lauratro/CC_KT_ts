d12b4240b508551873b15789de1b39ed9aeedbbb
lauratro <lauratronchin@hotmail.it>
2022-06-11 09:14:23 +0200

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,24 @@ import Login from "./components/Login/Login";
 import Header from "./components/Header/Header";
 import CoursesList from "./components/CoursesList/CoursesList";
 import VariableProvider from './context/VariableContext'
-
+import { BrowserRouter as Router, Routes, Route,Link } from "react-router-dom"
 import "./App.css";
 
 function App() {
   return (
-    <VariableProvider>
     <div className="App">
+    <Router>
+    <VariableProvider>
+
       <Header />
-      <Login />
-      <CoursesList />
-    </div>
+      <Routes>
+      <Route path= "/" element ={<Login />} />
+      <Route path= "/Courses" element ={<CoursesList />} />
+      
+      </Routes>
     </VariableProvider>
+    </Router>
+    </div>
   );
 }
 
